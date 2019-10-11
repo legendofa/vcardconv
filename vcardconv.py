15cd25e12bc0cbe955a8e82d622603dcac4108a1
legendofa <34162874+legendofa@users.noreply.github.com>
2019-10-11 17:07:11 +0200

    Update vcardconv.py

--- vcardconv.py
+++ vcardconv.py
@@ -8,7 +8,7 @@ contacts = re.sub(r'TEL;', r'TEL;TYPE=', contacts); #change syntax
 contacts = re.sub(r'TEL;TYPE=VOICE', r'TEL;TYPE=CELL', contacts); #unify mobile number tag
 contacts = re.sub(r'(.*)(\r?\n\1)', r'\1\n', contacts); #remove duplicate numbers
 contacts = re.sub(r';CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE', r'', contacts); #change syntax
-contacts = re.sub(r'(?:\n)FN.*', r'', contacts); #remove encoding tag
+contacts = re.sub(r'(?:\n)FN.*', r'', contacts); #remove FN attribute
 contacts = re.sub(r'\n\n', r'\n', contacts); #remove newlines
 
 res = [];
@@ -16,12 +16,12 @@ tee = 0;
 contacts = contacts.splitlines();
 for val in contacts: #foreach line
 	buff = val;
-	if re.search(r'((=(\d|A|B|C|D|E|F){2}){2,})', str(val)): #if contact info is encoded in hex
+	if re.search(r'((=(\d|A|B|C|D|E|F){2}){2,})', str(val)): #if line contains hex code
 		val = re.findall(r'((=(\d|A|B|C|D|E|F){2}){2,})', str(val));
 		val = [x[0] for x in val]; #get each first sublist value
 		i = 0;
 		while i < len(val): #convert to text foreach word
-			val[i] = re.sub(r'=', r'', val[i]); #remove equals
+			val[i] = re.sub(r'=', r'', val[i]); #remove equal signs
 			val[i] = bytearray.fromhex(val[i]).decode();
 			if i == 0: #substitute name
 				tee = re.sub(r':((=(\d|A|B|C|D|E|F){2}){2,})', ':'+val[i], buff);
